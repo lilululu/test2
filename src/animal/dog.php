bf1e617aef72a416b1682c684eed75a8a3fad3a4
[lilululu] <[2841502399@qq.com]>
2018-06-15 11:26:00 +0800

    first commit

---
+++ src/animal/dog.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 6/15/18
+ * Time: 11:09 AM
+ */
+
+namespace src\animal;
+
+class dog{
+
+    function say(){
+
+        echo "hello my name is wangcai\n";
+    }
+}
---
+++ src/flower/rose.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 6/15/18
+ * Time: 11:11 AM
+ */
+
+namespace src\flower;
+
+class rose{
+
+    function say(){
+
+        echo "my name is rose ,i am very beautiful\n";
+    }
+}
